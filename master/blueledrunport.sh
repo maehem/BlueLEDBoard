58746806d5315fa169a78a31c598784fd99bbf8e
Mark J Koch <maehem@users.noreply.github.com>
2022-05-28 15:00:46 -0700

    Add code comments.

--- master/blueledrunport.sh
+++ master/blueledrunport.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-# This script will start up a single instance of the LED master controller pointing to the specified device
+# This script will start up a single instance of the LED master
+# controller pointing to the specified device.
+#
 # note: only pass the device name and not the /dev directory
-# Typically called from startall
-# It kills any previously running instance on that device
-# Note that it seems like I should be using /var for all this stuff, but then this file would not
-# work when run by non-root
-
+#
+# Typically called from blueledrunall.sh
+#
+# Kill any previously running instance on that device
+# Note that it seems like I should be using /var for all this stuff,
+# but then this file would not work when run by non-root.
 if [ -e /tmp/blueled/$1/pid ]; then
   echo killing $(cat /tmp/blueled/$1/pid)
   kill $(cat /tmp/blueled/$1/pid)
@@ -15,10 +18,12 @@ if [ ! -e /tmp/blueled ]; then
     mkdir /tmp/blueled
 fi
 
+# Font might have already been copied by a previous instance.
 if [ ! -e /tmp/blueled/font.txt ]; then
     cp /etc/blueled/defaultfont.txt /tmp/blueled/font.txt
 fi
 
+# Make a dir for our ttyUSBn port.
 if [ ! -e /tmp/blueled/$1 ]; then
     mkdir /tmp/blueled/$1
 fi
@@ -32,7 +37,11 @@ if [ ! -e /tmp/blueled/$1/message.txt ]; then
     cp /etc/blueled/messages/$1/message.txt /tmp/blueled/$1/message.txt
 fi
 
-
+# The USB device must be the proper baud rate ( 1MHz ).
 stty -F /dev/$1 1000000 raw clocal -hupcl -echo
+# 'blueled' displays the message then reloads the message file.  This
+# way you can change the contents of /tmp/blueled/ttyUSBn/message.text
+# and it will display the latest file contents. The USB stick service
+# updates the message file once a minute.
 blueled /dev/$1  /tmp/blueled/$1/message.txt /tmp/blueled/font.txt &
 echo $! >/tmp/blueled/$1/pid
